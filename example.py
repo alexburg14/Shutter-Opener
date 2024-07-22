7df9b4065a95f66b879a9a98f02a47131675c0f0
Alex Burg Mac <alexburg14@gmail.com>
2024-07-22 15:05:07 +0200

    code works now, made example file

--- example.py
+++ example.py
@@ -1,3 +1,9 @@
 import shutter_opener as so
+import time
+so.get_serial_ports() # This will print the serial ports, then choose the one you want
 
-so.send_high('dev/cu.usbserial-1410')
+so.send_high("/dev/cu.usbserial-1420")
+
+time.sleep(2)
+
+so.send_low("/dev/cu.usbserial-1420")
--- shutter_opener.py
+++ shutter_opener.py
@@ -1,14 +1,16 @@
-import serial
+import serial,time
 import serial.tools.list_ports
 
 def send_high(serial_port):
     ser = serial.Serial(serial_port, 9600)
-    ser.write(b'HIGH\n')
+    time.sleep(2) # Serial needs a bit of time to initialize, 2s was the minimum that worked
+    ser.write(b"HIGH\n")
     print("Sent: HIGH")
 
 def send_low(serial_port):
     ser = serial.Serial(serial_port, 9600)
-    ser.write(b'LOW\n')
+    time.sleep(2) # Serial needs a bit of time to initialize, 2s was the minimum that worked
+    ser.write(b"LOW\n")
     print("Sent: LOW")
 
 def get_serial_ports():
@@ -16,3 +18,5 @@ def get_serial_ports():
 
     for port, desc, hwid in sorted(ports):
             print("{}: {} [{}]".format(port, desc, hwid))
+    return ports
+
